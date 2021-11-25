64d1df9f3e2ad7f5b4387d82ddb23fe7037db486
derpmaster45 <jmsteltz04@gmail.com>
2021-11-25 09:30:36 -0600

    New validaiton

--- IncomeCalculator/Calculator.cs
+++ IncomeCalculator/Calculator.cs
@@ -8,7 +8,7 @@ namespace IncomeCalculator
         {
             // Intro
             char choice = 'y';
-            Console.WriteLine("Hello and welcome to the income calculator!");
+            Console.WriteLine("Hello and welcome to the gross income calculator!");
             do
             {
 
@@ -19,8 +19,32 @@ namespace IncomeCalculator
                 // gather information about pay and hours worked
                 Console.WriteLine("How much do you make per hour?");
                 double.TryParse(Console.ReadLine(), out income);
+                //validation for the income
+                while (double.TryParse(Console.ReadLine(), out income) == false)
+                {
+                    Console.WriteLine("Please enter a vaild amount");
+                    double.TryParse(Console.ReadLine(), out income);
+                    if (income <= 0.00)
+                    {
+                        Console.WriteLine("Goodbye!!");
+                        System.Environment.Exit(0);
+                    }
+                }
+                 
                 Console.WriteLine("How may hours do you work per pay period?");
                 double.TryParse(Console.ReadLine(), out hoursWorked);
+                // validation for hours worked
+                while (double.TryParse(Console.ReadLine(), out hoursWorked) == false)
+                {
+                    Console.WriteLine("Please enter a vaild amount");
+                    double.TryParse(Console.ReadLine(), out hoursWorked);
+                       if (hoursWorked <= 0.00)
+                {
+                    Console.WriteLine("Goodbye!!");
+                    System.Environment.Exit(0);
+                }
+                }
+              
                 // calculate income based on the information passed by the user
                 double grossIncome = calculateGrossIncome(income, hoursWorked);
                 Console.WriteLine($"The gross income for this upcoming pay period is: {grossIncome.ToString("c")}");
@@ -29,6 +53,11 @@ namespace IncomeCalculator
             }
 
             while (choice == 'y');
+            if (choice == 'n') 
+            {
+                Console.WriteLine("Thanks for using the gross income calculator!");
+                System.Environment.Exit(0);
+            }
         }
         // functions
         public static double calculateGrossIncome(double income,double hoursWorked) 
