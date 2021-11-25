3cf006f9c50fe377eba1b209d51f5a8849de5acb
derpmaster45 <jmsteltz04@gmail.com>
2021-11-24 22:05:02 -0600

    Added loop capablilities

--- IncomeCalculator/Calculator.cs
+++ IncomeCalculator/Calculator.cs
@@ -7,23 +7,32 @@ namespace IncomeCalculator
         static void Main(string[] args)
         {
             // Intro
+            char choice = 'y';
             Console.WriteLine("Hello and welcome to the income calculator!");
-            // sets default values
-            double income = 0.00;
-            double hoursWorked = 0.00;
-            // gather information about pay and hours worked
-            Console.WriteLine("How much do you make per hour?");
-            double.TryParse(Console.ReadLine(), out income);
-            Console.WriteLine("How may hours do you work per pay period?");
-            double.TryParse(Console.ReadLine(), out hoursWorked);
-           double grossIncome= calculateGrossIncome(income, hoursWorked);
-            Console.WriteLine();
+            do
+            {
 
+
+                // sets default values
+                double income = 0.00;
+                double hoursWorked = 0.00;
+                // gather information about pay and hours worked
+                Console.WriteLine("How much do you make per hour?");
+                double.TryParse(Console.ReadLine(), out income);
+                Console.WriteLine("How may hours do you work per pay period?");
+                double.TryParse(Console.ReadLine(), out hoursWorked);
+                // calculate income based on the information passed by the user
+                double grossIncome = calculateGrossIncome(income, hoursWorked);
+                Console.WriteLine($"The gross income for this upcoming pay period is: {grossIncome}");
+            }
+            while (choice == 'y');
         }
+        // functions
         public static double calculateGrossIncome(double income,double hoursWorked) 
         {
             double grossIncome = income * hoursWorked;
             return grossIncome;
+
         }
     }
 }
